08ad7c762f45ef82974ccfe63b35515902372139
Hamdi Aziz <lolaziz.ca@gmail.com>
2025-04-17 15:54:10 +0200

    installed typeorm and connect the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: 'localhost', 
+    port: 27017, 
+    database: 'BD-Projects', 
+    synchronize: true, 
+    logging: true,
+  }),],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,]
 })
 export class AppModule {}
